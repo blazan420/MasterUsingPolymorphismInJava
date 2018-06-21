b3548bd5b63e9b1dbf07e137abc7a1346b685db3
kevin <kevinjobrien420@gmail.com>
2018-06-20 23:31:44 -0500

    226 App Number 27 - Part3- Master using Polymorphism in Java //Coding

--- app/src/main/java/com/example/kevin/masterusingpolymorphisminjava/Lion.java
+++ app/src/main/java/com/example/kevin/masterusingpolymorphisminjava/Lion.java
@@ -1,7 +1,52 @@
 package com.example.kevin.masterusingpolymorphisminjava;
 
-public class Lion {
+public class Lion extends Animal {
 
 
+    private boolean canFight = true;
+    private int fightingPower;
 
+    public Lion(String animalName, String animalColor, int power, int speed, boolean canFight,
+                int fightingPower) {
+
+        super(animalName, animalColor, power, speed);
+
+        if (fightingPower <= 0) {
+            throw new IllegalArgumentException("The Value of the Fighting Power must be greagter than" + " 0");
+        }
+
+        this.canFight = canFight;
+        this.fightingPower = fightingPower;
+
+    }
+
+
+    public boolean getCanFight() {
+        return canFight;
+    }
+
+    public void setCanFight(boolean canFight) {
+        this.canFight = canFight;
+    }
+
+    public int getFightingPower() {
+        return fightingPower;
+    }
+
+    public void setFightingPower(int fightingPower) {
+        this.fightingPower = fightingPower;
+    }
+
+
+    @Override
+    public int calculateTheOveralPowerOfAnimal() {
+        return super.calculateTheOveralPowerOfAnimal() + fightingPower;
+    }
+
+    @Override
+    public String toString() {
+        return  String.format("%s%n%s: %b%n%s: $d", super.toString(),
+                "Can our Lion fight?", getCanFight(), "The Fighting Power of our Lion is", getFightingPower());
+    }
 }
+
--- app/src/main/java/com/example/kevin/masterusingpolymorphisminjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/masterusingpolymorphisminjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.masterusingpolymorphisminjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        TextView txtAnimal = (TextView) findViewById(R.id.txtAnimal);
+        TextView txtLion = (TextView) findViewById(R.id.txtLion);
+        TextView txtPolymorphism = (TextView) findViewById(R.id.txtPolymorphism);
+
+        Animal animal = new Animal("Bear", "Black", -1, -1);
+        Lion lion = new Lion("myLion", "Yellow", -1, -1, true, 170);
 
 
     }
